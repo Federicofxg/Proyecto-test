b2b94d22c4fe5afc38f08a7aaf821101080b288c
Federico Xavier Garrido <130020035+Federicofxg@users.noreply.github.com>
2023-04-13 00:02:32 -0300

    maps

---
+++ pages/contact/script.js
@@ -0,0 +1,37 @@
+function validarDatos(){
+    $("#msgNombre").text(""); 
+    /*$("#msgApellido").text(""); */
+    $("#msgConsulta").text(""); 
+    $("#msgEmail").text("");
+    $("#msgAconsulta").text("");
+  
+  
+    let nombre =$("#nombre").val();
+    /*let apellido =$("#apellido").val();*/
+    let consulta =$("#consulta").val();
+    let email =$("#email").val();
+    let aconsulta =$("#aconsulta").val();
+    
+    if (nombre==""){
+      $("#msgNombre").text("Completar este campo"); 
+    }
+    /*if (apellido==""){
+      $("#msgApellido").text("Completar este campo"); 
+    }*/
+    if (consulta==""){
+      $("#msgConsulta").text("Completar este campo"); 
+    }
+    if (email==""){
+      $("#msgEmail").text("Completar este campo"); 
+    }
+    if (aconsulta==""){
+      $("#msgAconsulta").text("Completar este campo"); 
+    }
+   
+  else {alert("Consulta enviada")
+          
+    
+    }
+  
+  }
+  
