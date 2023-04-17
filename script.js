db92cc1add3b9452e0892443c860eb1216c3dbc5
Federico Xavier Garrido <130020035+Federicofxg@users.noreply.github.com>
2023-04-17 16:10:54 -0300

    add map point

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 43.5293101, lng: -5.6773233},
+          center: {lat: 43.542194, lng: -5.676875},
           zoom: 13
         });
       
